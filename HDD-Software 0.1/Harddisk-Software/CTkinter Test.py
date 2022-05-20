bce6870ff2abb4c0c4c4fa681df9f2eca04c4111
SteffenHakestad <Hakestad.steffen@gmail.com>
2022-05-20 16:28:31 +0200

    Experimenting with CustomTKinter

---
+++ HDD-Software 0.1/Harddisk-Software/CTkinter Test.py
@@ -0,0 +1,31 @@
+import tkinter
+import customtkinter
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+
+""""checkbox_window = customtkinter.CTk()
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("dark-blue")
+checkbox_window.title("Harddisk Datapartner")
+checkbox_window.iconbitmap(default='Harddisk-Software\\Logos\\HDDLOGO.ico')
+
+# Set app size and position on the screen (middle)
+app_width = 960
+app_height = 540
+screen_width = checkbox_window.winfo_screenwidth()
+screen_height = checkbox_window.winfo_screenheight()
+screen_x = (screen_width / 2) - (app_width / 2)
+screen_y = (screen_height / 2) - (app_height / 2)
+checkbox_window.geometry(f"{app_width}x{app_height}+{int(screen_x)}+{int(screen_y)}")
+
+# Title Frame
+title_frame = customtkinter.CTkFrame(master=checkbox_window, width=250, height=50, corner_radius=10)
+title_frame.pack(padx=20, pady=20)
+label = customtkinter.CTkLabel(master=title_frame, text="Velg Programvare", text_font=("Roboto Medium", -20))
+label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+background_frame = customtkinter.CTkFrame(master=, width=300, height=300, corner_radius=10, fg_color="green", bg_color="green")
+title_frame.pack"""
--- HDD-Software 0.1/Harddisk-Software/Checkbox Software.py
+++ HDD-Software 0.1/Harddisk-Software/Checkbox Software.py
@@ -1,25 +1,53 @@
+
 from tkinter import *
+import customtkinter
 import os
 import threading
 import subprocess
 
-checkbox_window = Tk()
-checkbox_window.title("Harddisk Datapartner")
-checkbox_window.iconbitmap(default='C:\\Users\\steffen\\Desktop\\HDD-Software 0.1\\Harddisk-Software\\Logos\\HDDLOGO.ico')
-checkbox_window.configure(background="Black")
+
+
+root = Tk()
+customtkinter.set_appearance_mode("dark") 
+customtkinter.set_default_color_theme("blue")
+root.title("Harddisk Datapartner")
+root.iconbitmap(default='Harddisk-Software\\Logos\\HDDLOGO.ico')
+root.configure(background="#383837")
 
 # Set app size and position on the screen (middle)
 app_width = 960
 app_height = 540
-screen_width = checkbox_window.winfo_screenwidth()
-screen_height = checkbox_window.winfo_screenheight()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 screen_x = (screen_width / 2) - (app_width / 2)
 screen_y = (screen_height / 2) - (app_height / 2)
-checkbox_window.geometry(f"{app_width}x{app_height}+{int(screen_x)}+{int(screen_y)}")
+root.geometry(f"{app_width}x{app_height}+{int(screen_x)}+{int(screen_y)}")
 
-Label (checkbox_window, text="Velg programvare", bg="Black", fg="white", font="Helvetica 20").grid(row=0, column=1)
+top_frame = customtkinter.CTkFrame(root, width=950, height=50, fg_color="#0b57c2", border_color="gray", border_width=0.5, corner_radius=10) # Blå
+center_frame = customtkinter.CTkFrame(root, width=800, height=400, fg_color="green", corner_radius=10) # 880x490
+left_frame = customtkinter.CTkFrame(root, width=80, height=450, fg_color="#4d4d4b", border_color="gray", border_width=0.5, corner_radius=10)
 
-software_list = "cmd /k "
+label = customtkinter.CTkLabel(top_frame, text="Test", text_color="silver", text_font=("Roboto", -30))
+label.place(relx=0.4, rely=0.1)
+
+def checksize():
+    print(app_height)
+    print(app_width)
+
+button1 = customtkinter.CTkButton(left_frame, command=checksize, text="1", width=70, height=70).place(relx=0.05, rely=0.03)
+button2 = customtkinter.CTkButton(left_frame, command=checksize, text="2", width=70, height=70).place(relx=0.05, rely=0.2)
+button3 = customtkinter.CTkButton(left_frame, command=checksize, text="3", width=70, height=70).place(relx=0.05, rely=0.37)
+button4 = customtkinter.CTkButton(left_frame, command=checksize, text="4", width=70, height=70).place(relx=0.05, rely=0.54)
+button4 = customtkinter.CTkButton(left_frame, command=checksize, text="5", width=70, height=70).place(relx=0.05, rely=0.71)
+"""root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)"""
+
+top_frame.grid(row=0, columnspan=1, rowspan=1, sticky="NE")
+"""center_frame.grid(sticky="SE")"""
+left_frame.grid(columnspan= 1, sticky="W")
+
+
+"""software_list = "cmd /k "
 vlc = IntVar()
 firefox = IntVar()
 unchecky = IntVar()
@@ -81,7 +109,7 @@ def install_selected_checkbox():
         print("No software selected")
     else:
         print("Subprosses installing")
-        """subprocess.call(software_list)"""
+        subprocess.Popen(software_list)
     
     # Unchecks all boxes
     vlc.set(0)
@@ -148,7 +176,8 @@ def uninstall_selected_checkbox():
         print("No software selected")
     else:
         print("Subprosses installing")
-        """subprocess.call(software_list)"""
+        subprocess.Popen(software_list, stdout=subprocess.PIPE)
+        linetest = software_list.stdout.readline()
     
     # Unchecks all boxes
     vlc.set(0)
@@ -164,34 +193,39 @@ def uninstall_selected_checkbox():
     software_list = "cmd /k "
 
 # Starts separate threads for installing and uninstalling packages
-x = threading.Thread(target=install_selected_checkbox, args=())
+x = threading.Thread(target=install_selected_checkbox)
 x.start()
-y = threading.Thread(target=uninstall_selected_checkbox, args=())
+y = threading.Thread(target=uninstall_selected_checkbox)
 y.start()
 
+
 #Software selection checkboxes
-vlc_image = PhotoImage(file="C:\\Users\\steffen\\Desktop\\HDD-Software 0.1\\Harddisk-Software\\Logos\\64x64\\VLC 64x64.png")
-vlc_check = Checkbutton(checkbox_window, text ="VLC", image=vlc_image, activebackground="gray", variable=vlc, onvalue=1, offvalue=0).grid(row=1, column=0)
+vlc_image = PhotoImage(file="Harddisk-Software\\Logos\\64x64\\VLC 64x64.png")
+vlc_check = Checkbutton(root, text ="VLC", image=vlc_image, activebackground="gray", variable=vlc, onvalue=1, offvalue=0).grid(row=1, column=0)
 
-firefox_image = PhotoImage(file="C:\\Users\\steffen\\Desktop\\HDD-Software 0.1\\Harddisk-Software\\Logos\\64x64\\Firefox 64x64.png")
-firefox_check = Checkbutton(checkbox_window, text ="Firefox", image=firefox_image, activebackground="gray", variable=firefox, onvalue=1, offvalue=0).grid(row=2, column=0)
+vlc_button = customtkinter.CTkButton(master=frame, image=vlc_image, text="", width=74, height=74, fg_color="#3f57bc").grid(row=1, column=0)
 
-spotify_image = PhotoImage(file="C:\\Users\\steffen\\Desktop\\HDD-Software 0.1\\Harddisk-Software\\Logos\\64x64\\Spotify 64x64.png")
-spotify_check = Checkbutton(checkbox_window, text ="Spotify", image=spotify_image, activebackground="gray", variable=spotify, onvalue=1, offvalue=0).grid(row=4, column=0)
+firefox_image = PhotoImage(file="Harddisk-Software\\Logos\\64x64\\Firefox 64x64.png")
+firefox_check = Checkbutton(root, text ="Firefox", image=firefox_image, activebackground="gray", variable=firefox, onvalue=1, offvalue=0).grid(row=2, column=0)
 
-teamwiever_image = PhotoImage(file="C:\\Users\\steffen\\Desktop\\HDD-Software 0.1\\Harddisk-Software\\Logos\\64x64\\Teamviewer 64x64.png")
-teamviewer_check = Checkbutton(checkbox_window, text ="Team Viewer", image=teamwiever_image, activebackground="gray", variable=teamviewer, onvalue=1, offvalue=0).grid(row=5, column=0)
+spotify_image = PhotoImage(file="Harddisk-Software\\Logos\\64x64\\Spotify 64x64.png")
+spotify_check = Checkbutton(root, text ="Spotify", image=spotify_image, activebackground="gray", variable=spotify, onvalue=1, offvalue=0).grid(row=4, column=0)
 
-steam_image = PhotoImage(file="C:\\Users\\steffen\\Desktop\\HDD-Software 0.1\\Harddisk-Software\\Logos\\64x64\\Steam 64x64.png")
-steam_check = Checkbutton(checkbox_window, text ="Steam", image=steam_image, activebackground="gray", variable=steam, onvalue=1, offvalue=0).grid(row=6, column=0)
+teamwiever_image = PhotoImage(file="Harddisk-Software\\Logos\\64x64\\Teamviewer 64x64.png")
+teamviewer_check = Checkbutton(root, text ="Team Viewer", image=teamwiever_image, activebackground="gray", variable=teamviewer, onvalue=1, offvalue=0).grid(row=5, column=0)
 
-discord_image = PhotoImage(file="C:\\Users\\steffen\\Desktop\\HDD-Software 0.1\\Harddisk-Software\\Logos\\64x64\\Discord 64x64.png")
-discord_check = Checkbutton(checkbox_window, text ="Discord", image=discord_image, activebackground="gray", variable=discord, onvalue=1, offvalue=0).grid(row=7, column=0)
+steam_image = PhotoImage(file="Harddisk-Software\\Logos\\64x64\\Steam 64x64.png")
+steam_check = Checkbutton(root, text ="Steam", image=steam_image, activebackground="gray", variable=steam, onvalue=1, offvalue=0).grid(row=6, column=0)
+
+discord_image = PhotoImage(file="Harddisk-Software\\Logos\\64x64\\Discord 64x64.png")
+discord_check = Checkbutton(root, text ="Discord", image=discord_image, activebackground="gray", variable=discord, onvalue=1, offvalue=0).grid(row=7, column=0)
 
 
 #Install and uninstall selected programs
-install_button = Button(checkbox_window, text="Installer valgte", command=install_selected_checkbox, width=0).grid(row=1, column=1)
-uninstall_button = Button(checkbox_window, text="Avinstaller valgte", command=uninstall_selected_checkbox, width=0).grid(row=2, column=1)
+install_button = customtkinter.CTkButton(root, text="Installer valgte", command=install_selected_checkbox, width=0, fg_color="#0b57c2", text_color="silver", text_font=("Roboto Medium", -20)).grid(row=10, column=2, padx=300, pady=5)
+uninstall_button = customtkinter.CTkButton(root, text="Avinstaller valgte", command=uninstall_selected_checkbox, width=0, fg_color="#0b57c2", hover_color="#f5681c", text_color="silver", text_font=("Roboto Medium", -20)).grid(row=12, column=2, padx=300, pady=5)
+"""
 
-checkbox_window.mainloop()
+
+root.mainloop()
 
